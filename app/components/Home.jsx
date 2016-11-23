b58d3e33a73265cdb549a6db7ba907334baff27c
Anthony Brown <awebrown@gmail.com>
2016-11-22 22:32:10 -0500

    copy edit

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -3,8 +3,8 @@ let React = require('react');
 let Home = React.createClass({
   render: function() {
     return (
-      <div>
-        <h3>Sefton Coffee Co.</h3>
+      <div className="main-text">
+        <h1>Sefton Coffee Co.</h1>
         <p>24 N 8th St <br /> Richmond, VA 23219</p>
         <p>Mon: 7:30 - 5 <br /> Tues - Fri: 7:30 - 7 <br /> Closed Weekends</p>
       </div>
